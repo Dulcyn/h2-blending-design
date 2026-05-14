e721630603db64782bb3f1495a9b8be79d8e0d0b
Lucas <l182775@dac.unicamp.br>
2026-05-14 15:02:06 -0300

    import all classes on opt

--- opt/__init__.py
+++ opt/__init__.py
@@ -4,20 +4,25 @@ import pyomo.environ as pyo
 
 
 from .electrolyzer import Electrolyzer
+from .hydrogen import Hydrogen
+from .water import Water
 from .tank import Tank
 from .grid import Grid
+from .gas import Gas
+
+
 
 class H2DesignOpt:
     def __init__(self, data):
         self.ez = Electrolyzer(data['electrolyzer'])
-        # self.ts = Grid(data['grid'])
-        # self.ht = Tank(data['tank'])
-        # self.
+        self.h2 = Hydrogen(data['hydrogen'])
+        self.wt = Water(data['water'])
+        self.ts = Grid(data['grid'])
+        self.ht = Tank(data['tank'])
+        self.gt = Gas(data['gas'])
 
         return
     
     def build(self):
         m = pyo.ConcreteModel()
         m.Ωt = pyo.Set(initialize=[t for t in range(0, 24)])
-
-        m.
