9ffe57126fdff96a8d5ea59e72ee79547157e12b
dulcy <dulcyavril@gmail.com>
2026-05-14 15:20:37 -0300

    update in the optm classes

---
+++ opt/general.py
@@ -0,0 +1,5 @@
+class General:
+    def __init__(self, data):
+        self.discount_rate = data['discount_rate']
+        self.lifetime      = data['lifetime']
+        self.h2_percentage = data['h2_percentage']
